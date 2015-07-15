9446337c01a9b3576dfaaaea04910545777a40e7
chaitanya edara <chaitanya.eedara@gmail.com>
2015-07-15 20:54:14 +0530

    dll hell example

---
+++ chaitanya_trainings/Assembly_example/dll_hell/dll_hell/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace dll_hell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("main clas");
+            Class1 ob=new Class1();
+            ob.write2();
+
+            Console.Read();
+
+        }
+    }
+}
