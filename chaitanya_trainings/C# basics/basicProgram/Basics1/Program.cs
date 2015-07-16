c8bfbc1ccb905be065c504ebe73ff48f6cb5bd0d
chaitanya edara <chaitanya.eedara@gmail.com>
2015-07-16 19:46:15 +0530

    c# basics oops

---
+++ chaitanya_trainings/C# basics/basicProgram/Basics1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basics1
+{
+
+   sealed class A
+    {
+        public virtual void m1()
+        {
+            Console.WriteLine("base");
+        }
+
+    }
+
+    class B : A
+    {
+        public override void m1()
+        {
+            Console.WriteLine("derived");
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A ob1;
+            ob1 = new A();
+            ob1.m1();
+            ob1 = new B();
+
+            ob1.m1();
+         
+           
+           
+            Console.Read();
+        }
+    }
+}
---
+++ chaitanya_trainings/C# basics/basicProgram/basicProgram/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicProgram
+{
+    public class Class1
+    {
+      //   StringBuilder  vs String
+
+    //    string a = "13";
+
+    ////    int b = int.Parse();
+
+    //    Console.WriteLine(a);;
+    }
+}
