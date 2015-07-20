5a3bdff8f5f56172bd46a411f8d944c4b9d1ea8d
chaitanya edara <chaitanya.eedara@gmail.com>
2015-07-20 07:56:50 +0530

    LINQ basic added

---
+++ chaitanya_trainings/c# data/LINQ/LINQ/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Data
+    {
+       public int a;
+       public int b;
+
+      public  Data(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // IEnumerable<Data> ob = new List<Data>();
+            List<Data> ob = new List<Data>();
+            ob.Add(new Data(1,2));
+            ob.Add(new Data(2, 2));
+            ob.Add(new Data(3, 2));
+            ob.Add(new Data(4, 2));
+
+            //LINQ
+
+            var max = ob.Max(x => x.a);
+
+            Console.Write(max);
+            Console.Read();
+
+        }
+    }
+}
