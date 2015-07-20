2a1a033de8e4e4297d20bf312a4d00e28ce565fd
chaitanya edara <chaitanya.eedara@gmail.com>
2015-07-20 20:05:19 +0530

    LINQ basics added

--- chaitanya_trainings/c# data/LINQ/LINQ/Program.cs
+++ chaitanya_trainings/c# data/LINQ/LINQ/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -24,16 +25,24 @@ namespace LINQ
         {
            // IEnumerable<Data> ob = new List<Data>();
             List<Data> ob = new List<Data>();
-            ob.Add(new Data(1,2));
+            ob.Add(new Data(1, 2));
             ob.Add(new Data(2, 2));
             ob.Add(new Data(3, 2));
             ob.Add(new Data(4, 2));
 
             //LINQ
 
-            var max = ob.Max(x => x.a);
+            var aitems = ob.Select(x => x.a==2);
+            int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, };
+          
+       
+            int maxnumber = arr.Max();
 
-            Console.Write(max);
+            //ArrayList
+
+
+
+          //  Console.Write(max);
             Console.Read();
 
         }
