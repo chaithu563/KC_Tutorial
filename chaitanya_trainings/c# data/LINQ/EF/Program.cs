47199a9344b4a331bf5a717dd435bbc3a3cdb6da
chaitanya edara <chaitanya.eedara@gmail.com>
2015-07-22 08:39:13 +0530

    entity frame work basic added

---
+++ chaitanya_trainings/c# data/LINQ/EF/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            chaitanyaEntities ob = new chaitanyaEntities();
+
+            var result = ob.Employees.Select(x => x.name);
+
+            foreach(var item in result)
+            {
+                Console.Write(item);
+
+            }
+
+            Console.Read();
+            
+        }
+    }
+}
